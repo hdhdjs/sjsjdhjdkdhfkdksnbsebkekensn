74a159b0e7c72170adcc3ecb7254add411907b07
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-01 00:29:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,27 +5,9 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-const devs = ['473980734109909007' , '' , '' , ''];
-const adminprefix = "'";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://m.twitch.tv/i00e");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
   console.log('')
   console.log('')
