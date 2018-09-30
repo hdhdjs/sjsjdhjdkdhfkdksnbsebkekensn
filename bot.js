cb41915784697042c90591cd51e1d3625ab32ac8
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-09-30 22:22:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,13 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-usertGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(` ‎سَآبقى‌‌كماأناوسلامًا على‌‌من ‌‌راق ‌‌لهُ‌الإبتعاد`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
+  con
   console.log('╔[════════════════════════════════════]╗');
   console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
@@ -27,29 +27,6 @@ usertGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-
-const devs = ['473980734109909007' , '' , '' , ''];
-const adminprefix = "'";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://m.twitch.tv/i00e");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
 
 
 client.login(process.env.BOT_TOKEN);
