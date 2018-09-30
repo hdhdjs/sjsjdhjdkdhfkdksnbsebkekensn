522a477647b5bcdd13c6c0690f349d3a3cd3e608
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-01 00:20:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
 
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 const devs = ['473980734109909007' , '' , '' , ''];
 const adminprefix = "'";
 client.on('message', message => {
@@ -11,19 +14,28 @@ client.on('message', message => {
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
       message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://m.twitch.tv/i00e");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
   }
-
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
 client.login(process.env.BOT_TOKEN);
