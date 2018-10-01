f6e7c12d439139a288cdbdb0f3192944a0a583f4
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-02 02:18:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,20 +15,20 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**!   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**!   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**!  ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://m.twitch.tv/i00e");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**!   ${argresult}**`)
   }
-  });
+  });  });
 
 client.login(process.env.BOT_TOKEN);
