0246677160b65911404987ff6b9b9377c8debe4f
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-02 01:56:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`I dont owe anybody so, fuck you :)`,"http://twitch.tv/S-F")
+
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,5 +27,29 @@ client.user.setGame(`I dont owe anybody so, fuck you :)`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+const devs = ['473980734109909007' , '' , '' , ''];
+const adminprefix = "'";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://m.twitch.tv/i00e");
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  }
+  });
 
 client.login(process.env.BOT_TOKEN);
