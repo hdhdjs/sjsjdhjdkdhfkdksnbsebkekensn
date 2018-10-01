dd91d307e6657f700408b17141fa80b68f861199
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-01 14:20:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(I dont owe anybody, so fuck you.‎‌‌‌,"http://twitch.tv/S-F")
+client.user.setGame(`I dont owe anybody so, fuck you :)`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
